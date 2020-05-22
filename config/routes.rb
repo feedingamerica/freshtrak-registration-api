3ba283f3ccc5315b3451699b9dc4252630dc724f
Basiltj07 <basiltj07@gmail.com>
2020-05-22 09:29:26 +0530

    Resolved the conflict while merging fromgithub

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 # frozen_string_literal: true
 
 Jets.application.routes.draw do
-
-  namespace :api do
-    resources :households
-  end
-
-  #Default homepage. This should be replaced.
-
   resources :guest_authentications, only: :create
 
   namespace :api do
     resources :reservations, only: %i[index show create delete]
     resource :user, only: %i[show update]
   end
-
 
   root 'jets/public#show'
 
