79f566087217a5b076b9223e5d0388fa22e23991
Cory Hahn <chahn010@gmail.com>
2020-05-29 09:46:12 -0400

    Removed unnecessary lib file / configs.

--- app/lib/config.rb
+++ app/lib/config.rb
@@ -1,10 +1,7 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-# Module for storing global configuration
+# # Module for storing global configuration
 module Config
     class << self
-      def freshtrak_registration_api_url
-        @freshtrak_registration_api_url ||= ENV.fetch('FRESHTRAK_REGISTRATION_API_URL')
-      end
     end
   end
