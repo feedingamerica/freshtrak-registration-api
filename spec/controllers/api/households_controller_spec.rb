3d59a79abc0c7538a26932e66207fcbcce698d00
Cory Hahn <chahn010@gmail.com>
2020-05-29 09:09:17 -0400

    Added .rspec to enable running spec files. Stubbed tests for households controller. Removed spec for household model.

--- spec/controllers/api/households_controller_spec.rb
+++ spec/controllers/api/households_controller_spec.rb
@@ -2,52 +2,30 @@
 
 describe Api::HouseholdsController, type: :controller do
     let(:freshtrak_registration_api) { instance_double(FreshtrakRegistrationApi) }
+    
     context 'with authenticated requests' do
-        # Will need a user object when auth becomes active
+        skip 'requires a user entity and auth'
         
         it 'adds a household for a user' do
-            skip
-            # params = {
-            #     household_name: 'Big Tester', 
-            #     household_number: 1,
-            #     address_attributes: {
-            #     address_line_1: "123 Test St",
-            #     address_line_2: "Suite 1",
-            #         city: "Irontown",
-            #         state: "OH",
-            #         zip_code: "45805",
-            #         zip_4: "12341"
-            #         }
-            #     }
-            # post '/api/households'
-            
-            # expect(response.status).to eq(200)
-            # expect(JSON.parse(response.body)).to eq(params)
         end
-
+    
         it 'shows the current user household' do
-            skip
         end
 
-        it 'updates the household' do
-            skip
-            # params = {
-            #     household_number: 5,
-            #     household_name: 'Turanga'
-            # }
-
-            # put '/api/households'
-        end
-
-        it 'updates the household address' do
-            skip
-            # params = {address_line_1: '123 Test St.', address_line_2:'suite 3', city: 'Irontown',
-            #           state: 'OH', zip_code: '45805', zip_4: '1234'}
-            # put '/api/households/'
+        it 'updates the current user household' do
         end
 
         it 'deletes a household' do
-            skip
+        end
+
+        it 'does not show another user household' do
+        end
+    end
+
+    context 'without authenticated requests' do
+        skip 'requires a user entity and auth'
+
+        it 'responds with unauthorized' do
         end
     end
 end    
