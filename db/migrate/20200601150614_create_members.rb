3f74f809c2fd9dbf9962e9b9621be8ab2971560e
Steve Bilogan <steve.bilogan@gmail.com>
2020-06-16 13:59:38 -0400

    rubospec

--- db/migrate/20200601150614_create_members.rb
+++ db/migrate/20200601150614_create_members.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
+# Migration to create the Household Members table
 class CreateMembers < ActiveRecord::Migration[6.0]
-    def change
-      create_table :members do |t|
-        t.belongs_to :household, foreign_key: true
-        t.belongs_to :user, foreign_key: true
-        t.integer :number
-        t.string :first_name, null: false
-        t.string :middle_name
-        t.string :last_name, null: false
-        t.date :date_of_birth, null: false
-        t.boolean :is_head_of_household, null: false, default: false
-        t.string :email, null: false, unique: true
-        t.boolean :is_active, null: false, default: true
-        t.string :added_by, null: false
-        t.timestamps
-      end
+  def change
+    create_table :members do |t|
+      t.belongs_to :household, foreign_key: true
+      t.belongs_to :user, foreign_key: true
+      t.integer :number
+      t.string :first_name, null: false
+      t.string :middle_name
+      t.string :last_name, null: false
+      t.date :date_of_birth, null: false
+      t.boolean :is_head_of_household, null: false, default: false
+      t.string :email, null: false, unique: true
+      t.boolean :is_active, null: false, default: true
+      t.string :added_by, null: false
+      t.timestamps
     end
   end
-  
+end
