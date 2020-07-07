0c12f09482918ba39b3150c40f8b5b2b91209e92
Mahesh Kurma <maheshruby405@gmail.com>
2020-07-07 16:53:36 -0400

    undo previous migration

--- db/migrate/20200616145716_add_event_slot_id_field_to_reservations.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Migration to add event_slot_id field to reservations table.
-class AddEventSlotIdFieldToReservations < ActiveRecord::Migration[6.0]
-  def change
-    add_column :reservations, :event_slot_id, :integer,
-               null: false, default: false
-  end
-end
