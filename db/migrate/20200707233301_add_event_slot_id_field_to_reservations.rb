ebfa3454eb10e83f83462b03c514b9cf76ac6483
Mahesh Kurma <maheshruby405@gmail.com>
2020-07-07 19:51:40 -0400

    fix lint

--- db/migrate/20200707233301_add_event_slot_id_field_to_reservations.rb
+++ db/migrate/20200707233301_add_event_slot_id_field_to_reservations.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Migration to add event_slot_id field to reservations table.
 class AddEventSlotIdFieldToReservations < ActiveRecord::Migration[6.0]
   def change
     add_column :reservations, :event_slot_id, :integer
