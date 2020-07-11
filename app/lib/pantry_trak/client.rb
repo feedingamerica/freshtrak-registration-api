1152438d801aa6fd5e7bdb1de63d34ad2861d173
Caroline Ibbotson <carolinem.ibb@gmail.com>
2020-07-10 22:10:45 -0400

    Create PantryTrak::Client

---
+++ app/lib/pantry_trak/client.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+module PantryTrak
+  class Client
+    def create_user(user)
+      raise 'Requires PantryTrak::User' unless user.instance_of? PantryTrak::User
+      raise user.errors.full_messages.to_sentence unless user.valid?
+
+      connection.post(create_user_path, user.to_json) do |req|
+        req.headers['Authorization'] = bearer_token
+      end.body
+    end
+
+    def create_reservation(id, user_id, event_date_id, event_slot_id)
+      payload = {
+        id: id,
+        user_id: user_id,
+        event_date_id: event_date_id,
+        event_slot_id: event_slot_id
+      }
+      connection.post(create_reservation_path, payload) do |req|
+        req.headers['Authorization'] = bearer_token
+      end.body
+    end
+
+    private
+
+    def connection
+      @connection ||= Faraday.new(base_url) do |c|
+        c.request :json
+        c.response :json
+        c.adapter Faraday.default_adapter
+      end
+    end
+
+    def bearer_token
+      payload = {
+        token: public_token,
+        time: Time.now.to_i
+      }
+      "Bearer #{JWT.encode(payload, hmac_secret, 'HS256', { typ: 'JWT' })}"
+    end
+
+    def hmac_secret
+      ENV['PANTRY_TRAK_SECRET']
+    end
+
+    def public_token
+      ENV['PANTRY_TRAK_TOKEN']
+    end
+
+    def base_url
+      ENV['PANTRY_TRAK_API_URL']
+    end
+
+    def create_user_path
+      if Jets.env.production?
+        'api/create_freshtrak_user.php'
+      else
+        'api/create_freshtrak_user_beta.php'
+      end
+    end
+
+    def create_reservation_path
+      if Jets.env.production?
+        'api/create_freshtrak_reservation.php'
+      else
+        'api/create_freshtrak_reservation_beta.php'
+      end
+    end
+  end
+end
---
+++ app/lib/pantry_trak/user.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module PantryTrak
+  class User
+    include ActiveModel::Model
+    include ActiveModel::Validations
+
+    attr_accessor :id, :user_type, :first_name, :middle_name, :last_name,
+      :suffix, :gender, :phone, :email, :address_line_1, :address_line_2,
+      :city, :state, :zip_code, :license_plate, :seniors_in_household,
+      :adults_in_household, :children_in_household, :permission_to_email,
+      :permission_to_phone, :date_of_birth, :identification_code
+
+    validates :id, :identification_code, presence: true
+  end
+end
