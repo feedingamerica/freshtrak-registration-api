75900a2fb78a34c09cafd6f4c8d1236a0ee6c909
mahesh kurma <maheshruby405@gmail.com>
2020-07-21 11:46:21 -0400

    Allow reservations to be made for event slots (#25)
    Co-authored-by: David Pickering <davepickering1@gmail.com>
    Co-authored-by: Nikhil Vengal <nikhil@boldpenguin.com>

--- app/services/create_reservation.rb
+++ app/services/create_reservation.rb
@@ -3,54 +3,78 @@
 # Creates an event_date reservation record for a user
 #   Ensures that the user has not already reserved a spot
 #   Ensures that the event_date has open capacity
+#   Ensures that the event_slot has open capacity
 class CreateReservation
-  attr_reader :user_id, :event_date_id, :reservation
+  attr_reader :user_id, :event_date_id, :event_slot_id, :reservation
 
-  def initialize(user_id:, event_date_id:)
+  def initialize(user_id:, event_date_id:, event_slot_id: nil)
     @user_id = user_id
     @event_date_id = event_date_id
+    @event_slot_id = event_slot_id
     @reservation = Reservation.new(user_id: user_id,
-                                   event_date_id: event_date_id)
+                                   event_date_id: event_date_id,
+                                   event_slot_id: event_slot_id)
   end
 
   def call
-    return failure if user_already_has_reservation?
+    begin
+      check_if_user_already_has_reservation
+      check_if_event_date_at_capacity
+      check_if_valid_event_slot
 
-    return failure if event_is_at_capacity?
+      return success if reservation.errors.empty? && reservation.save
+    rescue Faraday::Error => e
+      reservation.errors.add(:pantry_finder_api, e.message)
+    end
 
-    return failure unless reservation.save
-
-    success
-  rescue Faraday::Error => e
-    reservation.errors.add(:pantry_finder_api, e.message)
     failure
   end
 
   private
 
-  def user_already_has_reservation?
+  def check_if_user_already_has_reservation
     user_already_has_reservation =
       event_reservations.where(user_id: user_id).exists?
 
-    if user_already_has_reservation
-      reservation.errors.add(:user_id, 'has already registered for this event')
-    end
+    return unless user_already_has_reservation
 
-    user_already_has_reservation
+    reservation.errors.add(:user_id, 'has already registered for this event')
   end
 
-  def event_is_at_capacity?
-    event_is_at_capacity = event_reservations.count >= event_capacity
+  def check_if_event_date_at_capacity
+    event_date_at_capacity =
+      event_reservations.count >= event_date[:capacity]
 
-    if event_is_at_capacity
-      reservation.errors.add(:event_date_id, 'is at capacity')
-    end
+    return unless event_date_at_capacity
 
-    event_is_at_capacity
+    reservation.errors.add(:event_date_id, 'is at capacity')
   end
 
-  def event_capacity
-    PantryFinderApi.new.event_date(event_date_id)[:capacity]
+  def check_if_valid_event_slot
+    return unless event_slot_id
+
+    if event_slot && event_slot_at_capacity?
+      reservation.errors.add(:event_slot_id, 'is at capacity')
+    elsif !event_slot
+      reservation.errors.add(:event_slot_id, 'is not for event date')
+    end
+  end
+
+  def event_slot_at_capacity?
+    event_slot_reservations =
+      event_reservations.where(event_slot_id: event_slot_id)
+
+    event_slot_reservations.count >= event_slot[:capacity]
+  end
+
+  def event_date
+    @event_date ||= PantryFinderApi.new.event_date(event_date_id)
+  end
+
+  def event_slot
+    @event_slot ||=
+      event_date[:event_hours].map { |eh| eh[:event_slots] }.flatten
+                              .find { |es| es[:event_slot_id] == event_slot_id }
   end
 
   def event_reservations
---
+++ spec/lib/pantry_finder_api_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+describe PantryFinderApi do
+  let(:url) { 'http://test.com' }
+  let(:pantry_finder_api) { described_class.new(url: url) }
+  let(:stubs) { Faraday::Adapter::Test::Stubs.new }
+  let(:conn) { Faraday.new { |config| config.adapter :test, stubs } }
+
+  before do
+    allow(Faraday).to receive(:new).and_return(conn)
+  end
+
+  it 'returns event_date response' do
+    stubs.get('api/event_dates/123') do
+      [
+        200,
+        { 'Content-Type' => 'application/json;charset=utf-8' },
+        event_date_response
+      ]
+    end
+    response = pantry_finder_api.event_date('123')
+    response.should == event_date_response[:event_date]
+  end
+
+  def event_date_response
+    {
+      event_date:
+      {
+        id: 1742, event_id: 1740, capacity: 100,
+        accept_walkin: 1, accept_reservations: 0, accept_interest: 1,
+        start_time: '3 PM', end_time: '6 PM', date: '2020-07-30'
+      }
+    }
+  end
+end
