8e8fa983e7736edadfc9c8c52010ba955ef7352e
sarath-yerra <sarathpy.02@gmail.com>
2020-08-10 15:21:18 -0400

    added rspecs which are not covered

--- spec/controllers/guest_authentications_controller_spec.rb
+++ spec/controllers/guest_authentications_controller_spec.rb
@@ -8,6 +8,12 @@ describe GuestAuthenticationsController, type: :controller do
     expect(JSON.parse(response.body)['token']).not_to be_blank
   end
 
+  it 'returns no token on posts' do
+    allow_any_instance_of(User).to receive(:save) { false }
+    post '/guest_authentications'
+    expect(response.status).to eq(422)
+  end
+
   it 'creates a guest user on post' do
     expect { post '/guest_authentications' }
       .to change(User, :count).by(1)
