7aba63a96e596d676dc3fb6505119327bddc9b22
maheshkurma <maheshruby405@gmail.com>
2020-10-13 20:43:42 -0400

    twilio-ruby : send sms to user on event reisgter

---
+++ app/controllers/twilio_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Create a external API calls using Twilio.
+class TwilioController < ApplicationController
+  def sms
+    result = send_sms_to_recipent
+
+    if result.success?
+      render json: { sms_sent: true }
+    else
+      render json: {}, status: :unprocessable_entity
+    end
+  end
+
+  def send_sms_to_recipent
+    Twilio::Sms.new(params[:phone], params[:message]).call
+  end
+end
---
+++ spec/lib/twilio/sms_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+describe Twilio::Sms do
+  let(:twilio_rest_client) { instance_double(Twilio::REST::Client) }
+  let(:twilio_message) { instance_double('twilio message') }
+  let(:recipent_no) { '1234567890' }
+  let(:message) { 'Sucessfully registered' }
+
+  context 'with post request' do
+    before do
+      allow(Twilio::REST::Client).to receive(:new)
+        .and_return(twilio_rest_client)
+      allow(twilio_rest_client).to receive(:messages).and_return(twilio_message)
+      allow(twilio_message).to receive(:create).and_return true
+    end
+
+    it 'sends message to recipent' do
+      subject = described_class.new(recipent_no, message).call
+      expect(subject.success?).to eq(true)
+    end
+  end
+
+  context 'with invalid request' do
+    it "doesn't sends message to recipent" do
+      subject = described_class.new(recipent_no, message).call
+      expect(subject.success?).to eq(false)
+    end
+  end
+end
