a4e12b89b7e34e006ebeb05bb888d8f14087c487
sarath-yerra <sarathpy.02@gmail.com>
2020-12-02 14:14:25 -0500

    pushing lint fixes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
@@ -7,9 +9,9 @@
 #
 # set :output, "/path/to/my/cron_log.log"
 #
-every 5.minutes do
-  rake "some:great:rake:task"
-end
+# every 5.minutes do
+#   rake "some:great:rake:task"
+# end
 #
 # every 4.days do
 #   runner "AnotherModel.prune_old_records"
--- db/migrate/20201201020826_add_event_remainder_to_reservation.rb
+++ db/migrate/20201201020826_add_event_remainder_to_reservation.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Migration to set a flag for reminder message.
 class AddEventRemainderToReservation < ActiveRecord::Migration[6.0]
   def change
     add_column :reservations, :remainder_sent, :boolean, :default => false
