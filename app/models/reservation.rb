94a1e2bb92cfb34575e25fc4db4aad0a54d600d2
sarath-yerra <sarathpy.02@gmail.com>
2020-12-03 22:52:14 -0500

    Splitting script in to multiple methods

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,9 +1,95 @@
 # frozen_string_literal: true
 
 # Reserves a slot at an agency event for a user
+
+require 'net/http'
+
 class Reservation < ApplicationRecord
   belongs_to :user, inverse_of: :reservations
-  after_commit :sync_to_pantry_trak, on: :create
+  # after_commit :sync_to_pantry_trak, on: :create
+
+  def self.send_remainders
+    start_time = Time.current + 1.day
+    puts start_time
+    message = 'FreshTrak Remainder: You have successfully ' \
+      'registered for FreshTrak'
+    event_dates = {}
+
+    reservations = Reservation.where({ remainder_sent: false })
+    reservations.each do |reservation|
+      details = { reservation: reservation, event_dates: event_dates,
+                  start_time: start_time, message: message }
+      event_dates = send_reservstion_remainder(details)
+    end
+  end
+
+  def self.send_reservstion_remainder(details)
+    event_date_id = details[:reservation].event_date_id.to_s
+    event_dates = details[:event_dates]
+
+    unless event_dates[event_date_id]
+      event_dates = fetch_event_info(event_dates, event_date_id)
+    end
+
+    if event_dates[event_date_id]
+      details[:event_date_data] = event_dates[event_date_id]
+      check_and_send_remainder(details)
+    end
+
+    event_dates
+  end
+
+  def self.check_and_send_remainder(details)
+    phone = details[:reservation].user.try(:phone)
+
+    event_date_data = details[:event_date_data]
+    event_date = event_date_data[:event_date]
+
+    event_time = get_event_time(event_date)
+    send_sms =  event_time >= details[:start_time] &&
+                event_time <= (details[:start_time] + 4.minutes)
+
+    send_remainder(event_date_data[:twilio_phone_number], phone,
+                   details[:message], send_sms)
+  end
+
+  def self.get_event_time(event_date)
+    start_time = event_date['start_time']
+    time_string = start_time.include?(':') ? '%I:%M %p' : '%I %p'
+    Time.strptime("#{event_date['date']} #{start_time}",
+                  "%F #{time_string}")
+  end
+
+  def self.send_remainder(from, to, message, send_sms)
+    Twilio::Sms.new(from, to, message).call if to && send_sms
+  end
+
+  def self.fetch_event_info(event_dates, event_date_id)
+    uri = URI('http://localhost:8888/api/events?event_date_id=' + event_date_id)
+    res = Net::HTTP.get_response(uri)
+    event_dates[event_date_id] = false
+
+    if res.is_a?(Net::HTTPSuccess)
+      events = JSON.parse(res.body)['events']
+      unless events.empty?
+        return update_cached_event_details(event_dates, event_date_id, events)
+      end
+    end
+
+    event_dates
+  end
+
+  def self.update_cached_event_details(event_dates, event_date_id, events)
+    event_date = events[0]['event_dates'].try(:[], 0)
+    if event_date
+      event_dates[event_date_id] = {
+        event_date: event_date,
+        twilio_phone_number: events[0]['twilio_phone_number']
+      }
+    end
+
+    event_dates
+  end
 
   private
 
--- lib/tasks/event_remainder.rake
+++ lib/tasks/event_remainder.rake
@@ -1,61 +1,10 @@
+# frozen_string_literal: true
+
 require 'net/http'
 
-namespace :event do 
-	desc 'event remainder'
-	task :event_remainder => :environment do
-	  	puts "remainder messgae"
-	  	start_time = Time.current + 1.day
-	 	message = "FreshTrak Remainder: You have successfully registered for FreshTrak"
-
-	 	event_dates = {}
-	  	
-	  	reservations = Reservation.where({remainder_sent: false})
-		reservations.each do |reservation|
-			event_date_id = reservation.event_date_id.to_s
-
-			unless event_dates[event_date_id]
-				uri = URI("http://localhost:8888/api/events?event_date_id=" + event_date_id)
-				res = Net::HTTP.get_response(uri)
-				puts res.body.inspect
-				if res.is_a?(Net::HTTPSuccess)
-					events = JSON.parse(res.body)["events"]
-					if events.empty?
-						event_dates[event_date_id] = false
-					else
-						event_date = events[0]["event_dates"].try(:[], 0)
-						event_dates[event_date_id] = event_date ? {event_date: event_date, twilio_phone_number: events[0]['twilio_phone_number']} : false
-					end
-				else
-					event_dates[event_date_id] = false
-				end
-			end
-
-			user = reservation.user 
-			phone = user.phone
-			send_sms = false
-			puts "Phone Number #{user.phone}"
-
-
-			if event_dates[event_date_id]
-				event_date_data = event_dates[event_date_id]
-				event_date = event_date_data[:event_date]
-				event_start_time = event_date['start_time']
-				puts "event date #{event_date}"
-
-				event_time = Time.strptime("#{event_date['date']} #{event_start_time}", "%F #{event_start_time.include?(':') ? '%I:%M %p' : '%I %p' }")
-puts event_time
-puts start_time
-				send_sms = event_time >= start_time && event_time <= (start_time + 4.minutes)
-				puts "send_sms  #{send_sms} "
-
-				if phone && send_sms
-					Twilio::Sms.new(
-				      event_date_data[:twilio_phone_number] , phone, message
-				    ).call
-				    puts "Sending message to #{user.inspect}"
-				end
-			end
-		end
-	end
-
+namespace :event do
+  desc 'event remainder'
+  task event_remainder: :environment do
+    Reservation.send_remainders
+  end
 end
